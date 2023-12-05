133601df022d986c5679f9b2455971353657a033
ksilin <konstantin.siln@gmail.com>
2023-12-05 15:10:04 +0100

    addd test logging from cli

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ java {
 
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+    testLogging.showStandardStreams = true
 }
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
